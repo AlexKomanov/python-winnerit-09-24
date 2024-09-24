1f7edeb61cc8b13cd88edf39e79652055f7ba77c
AlexKomanov <akomanov88@gmail.com>
2024-09-24 21:25:48 +0300

    Adding logical_operator_and.py

---
+++ lesson_03/logical_operator_and.py
@@ -0,0 +1,11 @@
+# And
+print(f"True and True = {True and True}")
+print(f"False and True = {False and True}")
+print(f"True and False = {True and False}")
+print(f"False and False = {False and False}")
+print(f"True and True and False = {True and True and False}")
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+print(username == "user" and password == "qwerty")
