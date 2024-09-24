fbcd6ade81cc45d2f2ca1c6604b26b132ff2d0a1
AlexKomanov <akomanov88@gmail.com>
2024-09-24 20:19:31 +0300

    Adding lesson 3
    description - arithmetic operators

---
+++ lesson_03/arithmetic_operators.py
@@ -0,0 +1,22 @@
+a = 10
+b = 3
+
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} ** {b} = {a ** b}")
+print(f"{a} / {b} = {a / b}")
+print(f"{a} // {b} = {a // b}")
+print(f"{a} % {b} = {a % b}")
+
+print(abs(-500))
+print(min(10, 5, 100, -1))
+print(max(10, 5, 100, -1))
+print(pow(10, 3))
+
+print(round(3.45678))
+print(round(3.55678))
+print(round(3.55678, 2))
+print(round(3.55478, 2))
+
+print(9 ** 0.5)
---
+++ lesson_03/user_input.py
@@ -0,0 +1,38 @@
+
+# number_1 = int(input("Enter first number: "))
+# number_2 = float(input("Enter second number: "))
+#
+# print(number_1)
+# print(type(number_1))
+# print(number_2)
+# print(type(number_2))
+# print(number_2 + number_1)
+
+# name = input("Your name: ")
+# age = int(input("Your age: "))
+#
+# final_output = f"Name: {name} \nAge: {age + 1}"
+# print(final_output)
+# print(type(final_output))
+
+# num_1, num_2 = input("Enter numbers: ").split(",")
+# num_1_int = int(num_1)
+# num_2_int = int(num_2)
+# print(num_1_int, type(num_1_int))
+# print(num_2_int, type(num_2_int))
+
+# num_2 = input("Enter numbers: ").split(",")
+# print(num_2, type(num_2))
+
+# list_of_strings = input("enter: ").split()
+# print(list_of_strings, type(list_of_strings))
+# list_of_unclear = map(int, list_of_strings)
+# print(list_of_unclear, type(list_of_unclear))
+# final_list = list(list_of_unclear)
+# print(final_list, type(final_list))
+
+list_of_strings = input("enter: ").split()
+num1, num2, num3 = map(int, list_of_strings)
+print(num1)
+print(num2, type(num2))
+print(num3)
