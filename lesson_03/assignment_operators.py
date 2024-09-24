8783c32b0c4432933e77867695d995b922ef2b99
AlexKomanov <akomanov88@gmail.com>
2024-09-24 20:40:21 +0300

    Adding assignment operators

---
+++ lesson_03/assignment_operators.py
@@ -0,0 +1,20 @@
+a = 20
+print(f'{a = }')
+a += 50
+print(f'{a = }')
+a -= 10
+print(f'{a = }')
+a *= 5
+print(f'{a = }')
+a /= 29
+print(f'{a = }')
+a //= 2
+print(f'{a = }')
+a **= 3
+print(f'{a = }')
+a %= 7
+print(f'{a = }')
+
+# a++ a--
+a+=1
+a-=1
