69f855d2c8090cf742eaef832d7e459028e33d76
AlexKomanov <akomanov88@gmail.com>
2024-11-13 00:14:40 +0200

    Lesson 8 - test with fixtures

--- tests/test_accum.py
+++ tests/test_accum.py
@@ -1,12 +1,17 @@
 from source.accum import Accumulator
+import pytest
 
 def test_accum_creation():
     accum = Accumulator()
     assert accum.count == 0
 
-
+@pytest.mark.winnerit
 def test_add_counts_twice():
     accum = Accumulator()
     accum.add_counts()
     accum.add_counts()
     assert accum.count == 2
+
+def test_add_counts_with_params(global_accum):
+    global_accum.add_counts(10)
+    assert global_accum.count == 10
