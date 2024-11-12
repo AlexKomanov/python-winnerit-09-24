3e9c670063515a06bd944662d34473456a024414
AlexKomanov <akomanov88@gmail.com>
2024-11-12 20:43:48 +0200

    Lesson 8 - pytest parametrized csv file

--- lesson_07/test_05_parametrized.py
+++ lesson_07/test_05_parametrized.py
@@ -22,6 +22,7 @@ test_data = [(1, 5, 6), (10, 5, 15), (1, 0, 1), (20, 40, 60), (10, 500, 510), (-
 
 @pytest.mark.parametrize("num_1, num_2, result", test_data)
 def test_sum_parameters(num_1, num_2, result):
+    print(f"Starting test with {num_1}, {num_2}, {result}")
     assert num_1 + num_2 == result
 
 
